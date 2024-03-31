4b5c9ce37c78511c18b8fc44881b05f3dc4feb15
Laurens Kruis <laurens@kruis.name>
2024-03-31 16:53:09 +0200

    Added simple gimos rendering

--- Runtime/Utility/BodyTreeUtility.Matrix.cs
+++ Runtime/Utility/BodyTreeUtility.Matrix.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace MischievousByte.Masquerade
+namespace MischievousByte.Masquerade.Utility
 {
-    public static class BodyTreeUtility
+    public static partial class BodyTreeUtility
     {
         public static void ChangeSpace(this in BodyTree<Matrix4x4> source, Space target, out BodyTree<Matrix4x4> destination)
         {
-            destination = new();
-            destination[BodyNode.Sacrum] = source[BodyNode.Sacrum];
+            var tmp = new BodyTree<Matrix4x4>();
+            tmp[BodyNode.Sacrum] = source[BodyNode.Sacrum];
 
             var selection = (BodyNode.All ^ BodyNode.Sacrum).Enumerate();
             if (target == Space.World)
                 foreach (var node in selection)
-                    destination[node] = destination[node.Previous()] * source[node];
+                    tmp[node] = tmp[node.Previous()] * source[node];
             else
                 foreach (var node in selection)
-                    destination[node] = destination[node.Previous()].inverse * source[node];
+                    tmp[node] = source[node.Previous()].inverse * source[node];
+
+            destination = tmp;
         }
     }
 }
